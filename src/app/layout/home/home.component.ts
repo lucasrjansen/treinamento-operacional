702853e2bca33448e46ab6603c3a69e5a624a2d2
Lucas Janzen <lucasrainoldojansen@gmail.com>
2020-03-20 09:08:30 -0300

    Finalizado Dashboards

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -14,9 +14,12 @@ export class HomeComponent implements OnInit {
     dadosEncomendasPorMes: DashboardOptions;
     dadosEncomendasPorEntregador: DashboardOptions;
     dadosProblemasPorEntregador: DashboardOptions;
+    dadosEncomendasPorEstado: DashboardOptions;
+    dadosEncomendasPorCidade: DashboardOptions;
+    dadosEncomendasPorCliente: DashboardOptions;
 
     carregar = false;
-    
+
     constructor(private dashboardService: DashboardService) { }
 
     ngOnInit() {
@@ -37,16 +40,16 @@ export class HomeComponent implements OnInit {
     }
 
     trocarGrafico(iconId: string) {
-        
+
         let icon = this.buscarElemento(iconId);
         icon.textContent = icon.textContent.trim() == 'pie_chart' ? 'bar_chart' : 'pie_chart';
     }
 
     validarGrafico(iconeGrafico: string, iconId: string): Boolean {
-        
+
         let icon = this.buscarElemento(iconId);
-       
-        if(iconeGrafico.trim() != icon.textContent.trim()){
+
+        if (iconeGrafico.trim() != icon.textContent.trim()) {
             return true
         }
         return false
@@ -72,8 +75,22 @@ export class HomeComponent implements OnInit {
             dataSource: problemasPorEntregador
         }
 
-        console.log('montarDashboardDados');
-        console.log(this.dadosEncomendasPorEntregador);
+        this.dadosEncomendasPorEstado = {
+            infos: this.montarDashboardInfo(),
+            valueId: 'nome',
+            dataSource: encomendasPorEstado
+        }
+
+        this.dadosEncomendasPorCidade = {
+            infos: this.montarDashboardInfo(),
+            valueId: 'nome',
+            dataSource: encomendasPorCidade
+        }
+        this.dadosEncomendasPorCliente = {
+            infos: this.montarDashboardInfo(),
+            valueId: 'nome',
+            dataSource: encomendasPorCliente
+        }
     }
 
     montarDashboardInfo(): DashboardInfo[] {
@@ -88,6 +105,58 @@ export class HomeComponent implements OnInit {
         return dashboardInfo;
     }
 }
+
+export const encomendasPorCliente = [
+    {
+        nome: "Joao silva",
+        quantidade: 2
+    },
+    {
+        nome: "Nelson Janzen",
+        quantidade: 4
+    },
+    {
+        nome: "Rafael Santos",
+        quantidade: 1
+    },
+    {
+        nome: "Gabriel Mondini",
+        quantidade: 3
+    }
+]
+export const encomendasPorCidade = [
+    {
+        nome: "Apiúna",
+        quantidade: 6
+    },
+    {
+        nome: "Ascurra",
+        quantidade: 5
+    },
+    {
+        nome: "Blumenau",
+        quantidade: 36
+    },
+    {
+        nome: "Timbó",
+        quantidade: 34
+    }
+]
+
+export const encomendasPorEstado = [
+    {
+        nome: "Santa Catarina",
+        quantidade: 22
+    },
+    {
+        nome: "Rio Grande do Sul",
+        quantidade: 31
+    },
+    {
+        nome: "Paraná",
+        quantidade: 36
+    }
+]
 
 export const problemasPorEntregador = [
     {
